40fafc36b6bef192dc507380c6e747370dce386d
Niemur Obi <niemur.rahman30@gmail.com>
2022-03-10 17:59:34 -0500

    adding slider from right

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,20 @@
 $(document).ready(() => {
+
     $('.project-header').click(function() {
-        $('.project-desc').toggle();
+        $('.project-desc').animate({
+            width: "toggle",
+
+        }, 1000);
+    });
+
+
+
+
+    $('.project-header1').click(function() {
+        $('.project-desc1').animate({
+            width: "toggle",
+
+        }, 1000);
     });
 
     // $('.project-header').on('click', event => {
