f8669dbd494a995ca2995a47aa5484e308df1ebb
Niemur Obi <niemur.rahman30@gmail.com>
2022-03-09 21:40:21 -0500

    adding toogle

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $('.project-header').click(function() {
+        $('.project-desc').toggle();
+    });
+
+    // $('.project-header').on('click', event => {
+    //     $(event + '.project-desc').addClass('hideDesc');
+    // });
+
+});
